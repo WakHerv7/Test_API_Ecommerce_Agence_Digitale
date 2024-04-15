9a4b9cb92ce413347aad65ff92fbbdaff316f805
WakHerv7 <wakam.hermann@gmail.com>
2024-04-15 01:42:09 +0100

    move-to-sqlite3-db

--- config/databasecopy.ts
+++ config/databasecopy.ts
@@ -6,7 +6,6 @@
  */
 
 import Env from '@ioc:Adonis/Core/Env'
-import Application from '@ioc:Adonis/Core/Application'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
 const databaseConfig: DatabaseConfig = {
@@ -34,31 +33,18 @@ const databaseConfig: DatabaseConfig = {
     | npm i mysql
     |
     */
-    sqlite: {
-      client: 'sqlite',
+    mysql: {
+      client: 'mysql',
       connection: {
-        filename:Application.tmpPath('db.sqlite3')
+        host: Env.get('MYSQL_HOST'),
+        port: Env.get('MYSQL_PORT'),
+        user: Env.get('MYSQL_USER'),
+        password: Env.get('MYSQL_PASSWORD', ''),
+        database: Env.get('MYSQL_DB_NAME'),
       },
-      migrations:{
-        naturalSort:true
-      },
-      useNullAsDefault: true,
       healthCheck: false,
 			debug: false,
     },
-    
-    // mysql: {
-    //   client: 'mysql',
-    //   connection: {
-    //     host: Env.get('MYSQL_HOST'),
-    //     port: Env.get('MYSQL_PORT'),
-    //     user: Env.get('MYSQL_USER'),
-    //     password: Env.get('MYSQL_PASSWORD', ''),
-    //     database: Env.get('MYSQL_DB_NAME'),
-    //   },
-    //   healthCheck: false,
-		// 	debug: false,
-    // },
 
   },
 
